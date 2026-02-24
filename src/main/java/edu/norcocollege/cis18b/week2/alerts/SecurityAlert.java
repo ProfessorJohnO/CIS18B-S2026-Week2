4c680a89ddde230aa8d326d1fb67da999cb63086
Daniel Rios <danieljrm100873@gmail.com>
2026-02-24 23:05:36 +0000

    Convert SecurityAlert class to Java record

--- src/main/java/edu/norcocollege/cis18b/week2/alerts/SecurityAlert.java
+++ src/main/java/edu/norcocollege/cis18b/week2/alerts/SecurityAlert.java
@@ -1,17 +1,11 @@
 package edu.norcocollege.cis18b.week2.alerts;
 
-/**
- * TODO:
- *  - Convert this class into a Java 17+ record
- *  - Include the following fields:
- *      String id
- *      String sourceSystem
- *      String severity
- *      String description
- *      long timestamp
- *
- *  - (Optional stretch) Add validation in a compact constructor
- */
-public class SecurityAlert {
-    // TODO: Replace this entire class with a record
-}
+// Replacing public class "SecurityAlert" with a record to create immutable data classes
+public record SecurityAlert
+(
+    String id,
+    String sourceSystem,
+    String severity,
+    String description,
+    long timestamp
+) {}
