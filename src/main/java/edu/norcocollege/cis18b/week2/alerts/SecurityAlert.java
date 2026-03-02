bc0a04ad21cfcd64ae09f3243ffd50084af302c3
llacidrainll <gdgarcia1209@gmail.com>
2026-03-02 20:26:21 +0000

    added alert and alert by Id using Optional

--- src/main/java/edu/norcocollege/cis18b/week2/alerts/SecurityAlert.java
+++ src/main/java/edu/norcocollege/cis18b/week2/alerts/SecurityAlert.java
@@ -12,6 +12,15 @@ package edu.norcocollege.cis18b.week2.alerts;
  *
  *  - (Optional stretch) Add validation in a compact constructor
  */
-public class SecurityAlert {
-    // TODO: Replace this entire class with a record
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.List;
+public record SecurityAlert (String id, String sourceSystem, String severity, String description, long timestamp) {}
+public class Main {
+    public static void main(String[] args) {
+        SecruityAlert A = new
+
+
+
+    }
 }
