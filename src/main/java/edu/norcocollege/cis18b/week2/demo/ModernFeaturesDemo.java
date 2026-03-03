61247dbb59766096c51fc850f0fa7ce0fe864ee1
siddiquiabdullah526-wq <siddiquiabdullah526@gmail.com>
2026-03-03 02:57:00 +0000

    ModernFeaturesDemo.java

--- src/main/java/edu/norcocollege/cis18b/week2/demo/ModernFeaturesDemo.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/ModernFeaturesDemo.java
@@ -1,18 +1,3 @@
-//package edu.norcocollege.cis18b.week2.demo;
-
-/**
- * This class is a review of basic Java language features. It’s not
- * meant to be comprehensive, but it should cover the basics and provide
- * a refresher on syntax and concepts.
- **/
-//public class ModernFeaturesDemo {
-
-    //public void demonstrate() {
-        // TODO: Implement demonstration of modern Java features
-        // NOTE: Remove the exception below when you've implemented your code
-        //throw new UnsupportedOperationException("Not implemented yet");
-    //}
-//}
 
 package edu.norcocollege.cis18b.week2.demo;
 
@@ -32,7 +17,7 @@ import java.util.Optional;
  */
 public class ModernFeaturesDemo {
 
-    // Record type (modern Java feature)
+    // Record type 
     record User(String username, int level) {}
 
     public void demonstrate() {
@@ -42,20 +27,20 @@ public class ModernFeaturesDemo {
         Optional<String> maybeName = Optional.of("Jeff");
         System.out.println("Optional example: " + maybeName.orElse("Unknown"));
 
-        // Create a list of records
+        // Make a list of records
         var users = List.of(
                 new User("alpha", 1),
                 new User("bravo", 2),
                 new User("charlie", 3)
         );
 
-        // Stream operation (filter + count)
+        // Stream operation
         var highLevelCount = users.stream()
                 .filter(u -> u.level() >= 2)
                 .count();
         System.out.println("Stream example: users with level >= 2 = " + highLevelCount);
 
-        // Pattern matching with instanceof
+        // pattern matching with instanceof
         Object obj = users.get(0);
         if (obj instanceof User u) {
             System.out.println("instanceof pattern match: " + u.username() + " lvl " + u.level());
