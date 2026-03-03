c454056605c948072c00435973e78f8a62dd2123
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-03 07:03:49 +0000

    added record and matcing pat with instaceof

--- src/main/java/edu/norcocollege/cis18b/week2/demo/ModernFeaturesDemo.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/ModernFeaturesDemo.java
@@ -8,9 +8,21 @@ import java.util.stream.Collectors;
  * ModernFeaturesDemo
  */
 public class ModernFeaturesDemo {
-
     public void demonstrate() {
-       
-        
+
+        // ----------------------------
+        // 1. Record type
+        // ----------------------------
+        record Dinosaur(String name, boolean carnivore, int age) {}
+        var ankylosaurus = new Dinosaur("Ankylosaurus", false, 68);
+
+        System.out.println("Record Dinosaur: " + ankylosaurus);
+
+        // ----------------------------
+        // 2. Pattern matching with instanceof
+        // ----------------------------
+        Object obj = ankylosaurus;
+        if (obj instanceof Dinosaur dino) {
+            System.out.println(dino.name() + " is " + (dino.carnivore() ? "a carnivore" : "an herbivore"));  
     }
 }
