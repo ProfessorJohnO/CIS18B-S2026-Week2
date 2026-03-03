685e30d3972e02a79808b0f6ffe4b522c1d0f33d
GamingPriest <kclang02@gmail.com>
2026-03-03 04:26:38 +0000

    Wrote code for Language basic review. had to troubleshoot MVN found out it wasnt installed.

--- src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
+++ src/main/java/edu/norcocollege/cis18b/week2/demo/LanguageBasicsReview.java
@@ -1,21 +1,92 @@
 package edu.norcocollege.cis18b.week2.demo;
+import java.util.List;
 
-/**
- * This class is a review of basic Java language features. It’s not
- * meant to be comprehensive, but it should cover the basics and provide
- * a refresher on syntax and concepts.
- *
- * TODO:
- *  - Demonstrate variable declarations and types
- *  - Demonstrate control flow (if, switch, loops)
- *  - Demonstrate methods and parameters
- *  - Demonstrate basic OOP concepts (classes, objects, inheritance)
- */
-public class LanguageBasicsReview {
+public class LanguageBasicsReview 
+{
+    
+    // exceptions
+    static class InvalidNumberException extends Exception 
+    {
+        public InvalidNumberException(String message) 
+        {
+            super(message);
+        }
+    }
+    
+    //instance
+    public void greet(String name)
+    {
+        System.out.println("Hello, " + name);
+    }
 
-    public void demonstrate() {
-        // TODO: Implement demonstration of basic Java features
-        // NOTE: Remove the exception below when you've implemented your code
-        throw new UnsupportedOperationException("Not implemented yet");
+    //static
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    //overload
+    public static double add(double a, double b)
+    {
+        return a + b;
+    }
+
+    //mondern switch
+    public static String checkNumber(int number)
+    {
+        return switch (number)
+        {
+            case 0 -> "Zero";
+            case 1, 2, 3 -> "Small Number";
+            default -> "Large number";
+        };
+    }
+
+    //throws custom exceptions
+    public static void validateNumber(int number) throws InvalidNumberException
+    {
+        if (number < 0)
+        {
+            throw new InvalidNumberException("Number cannot be negative.");
+        }
+    }
+
+    public void demonstrate() 
+    {
+        //var demonstration
+        var number = 5;
+        var message = "Java Review";
+
+        System.out.println("Number: " + number);
+        System.out.println("Message: " + message);
+
+        //instance method call
+        greet("Student");
+
+        //static + overload
+        System.out.println("Add ints: " + add(3, 4));
+        System.out.println("Add doubles: " + add(3.5, 4.5));
+
+        //list.of
+        var numbers = List.of(1, 2, 3, 4);
+        System.out.println("List: " + numbers);
+
+        //try/catch custom expceptions
+        try 
+        {
+            validateNumber(-1);
+        }
+        catch (InvalidNumberException e)
+        {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+        //swtich expression
+        System.out.println("Check number: " + checkNumber(2));
+    }
+
+    public static void main(String[] args) 
+    {
+       var review = new LanguageBasicsReview();
+       review.demonstrate();
     }
 }
